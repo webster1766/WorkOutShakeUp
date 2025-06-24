117b837293d3a623ba85f62274e69780b41ec6e6
Steve Webster <SteveW@dhicorp.com>
2025-06-24 16:04:01 -0600

    update for mongodb ver 8.0

--- IntegrationTests/Program.cs
+++ IntegrationTests/Program.cs
@@ -3,8 +3,10 @@ using MongoDB.Driver;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using WosiDomain;
 using WosiDomain.MongoDocs;
 
 namespace IntegrationTests
@@ -17,21 +19,26 @@ namespace IntegrationTests
     {
       var client = new MongoClient("mongodb://localhost:27017");
       mongoDb = client.GetDatabase("wosu");
-      var movements = mongoDb.GetCollection<MovementDoc>("movements");
-      var bodyparts = mongoDb.GetCollection<BodyPartDoc>("bodyparts");
-      var equipment = mongoDb.GetCollection<EquipmentDoc>("equipment");
 
       //await collection.InsertOneAsync(new BsonDocument("Name", "Jack"));
 
       //var list = await collection.Find(new BsonDocument("Name", "Jack"))
       //    .ToListAsync();
 
-      var query = movements.AsQueryable();
-      var list = query.ToList();
-      foreach (var document in list)
-      {
-        Console.WriteLine(document.Name);
-      }
+      Console.WriteLine("MOVEMENTS");
+      var movements = mongoDb.GetCollection<MovementDoc>("movements");
+      var mlist = GetList<MovementDoc>(movements);
+      PrintList(mlist);
+
+      Console.WriteLine("BODYPARTS");
+      var bodyparts = mongoDb.GetCollection<BodyPartDoc>("bodyparts");
+      var blist = GetList(bodyparts);
+      PrintList(blist);
+
+      Console.WriteLine("EQUIPMENT");
+      var equipment = mongoDb.GetCollection<EquipmentDoc>("equipment");
+      var elist = GetList(equipment);
+      PrintList(elist);
 
       Console.WriteLine("Press [Enter] to exit");
       Console.ReadLine();
@@ -61,5 +68,36 @@ namespace IntegrationTests
       }
     }
 
+    private static List<T> GetList<T>(IMongoCollection<T> collection)
+    {
+      var query = collection.AsQueryable();
+      var list = query.ToList();
+
+      return list;
+    }
+
+    private static void PrintList<T>(List<T> list)
+    {
+      list.ForEach(x =>
+      {
+        if (typeof(T) == typeof(MovementDoc))
+        {
+          var doc = x as MovementDoc;
+          Console.WriteLine(doc.Name);
+        }
+        else if (typeof(T) == typeof(BodyPartDoc))
+        {
+          var doc = x as BodyPartDoc;
+          Console.WriteLine(doc.Name);
+        }
+        else if (typeof(T) == typeof(EquipmentDoc))
+        {
+          var doc = x as EquipmentDoc;
+          Console.WriteLine(doc.Description);
+        }
+      });
+
+      Console.WriteLine();
+    }
   }
 }
